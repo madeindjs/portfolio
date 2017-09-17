f8fe527924270fb16241f44540b8bbcff5e9176b
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2017-09-17 16:05:13 +0200

    Redesign global site

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh pi 'rm -r /var/www/nature_coiffure/*'
-jekyll build && scp -r _site/* pi:/var/www/nature_coiffure 
+ssh pi 'rm -r /var/www/portfolio/*'
+jekyll build && scp -r _site/* pi:/var/www/portfolio 
