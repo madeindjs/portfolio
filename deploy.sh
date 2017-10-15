b6116b4f0e69541b993b2e40a30720b32dffd1a3
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2017-10-15 15:22:34 +0200

    update host

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh pi 'rm -r /var/www/portfolio/*'
-bundle exec jekyll build && scp -r _site/* pi:/var/www/portfolio 
+ssh pi2 'rm -r /var/www/portfolio/*'
+bundle exec jekyll build && scp -r _site/* pi2:/var/www/portfolio 
