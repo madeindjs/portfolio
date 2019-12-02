c05cf9a286f7040fb9e0dd61b184edda7ee32e46
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-12-02 13:37:06 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 git fetch origin master
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
   git pull origin master
-  bundle exec jekyll build
+  bundle exec jekyll build --incremental
   cp -r _site/* /var/www/portfolio/
 fi
