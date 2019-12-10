99820dac3da4f776ca89695bfa1f7429e2c6c9f0
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-12-10 13:05:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-git fetch origin master
-if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+  echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+  echo "Need to pull"
   git pull origin master
   bundle exec jekyll build --incremental
   cp -r _site/* /var/www/portfolio/
+elif [ $REMOTE = $BASE ]; then
+  echo "Need to push"
+else
+  echo "Diverged"
 fi
