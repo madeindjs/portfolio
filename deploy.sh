36d37e2ec74fbd96cc1997c4761b4f78f1d01e63
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-02-28 14:15:39 +0100

    Correct syntax error

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,9 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $REMOTE ]; then
-  # echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-  # echo "Need to pull"
+if [ $LOCAL = $BASE ]; then
   git pull origin master
   /home/pi/.rvm/gems/ruby-2.4.0/wrappers/jekyll build
   rm -rf /var/www/portfolio/*
   cp -r _site/* /var/www/portfolio/
-elif [ $REMOTE = $BASE ]; then
-  # echo "Need to push"
-else
-  # echo "Diverged"
 fi
