caab4061b89e4f7ccfd2d16d087bb9c7d5c507c1
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-02-28 14:10:44 +0100

    Remove log

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,15 @@ REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
 if [ $LOCAL = $REMOTE ]; then
-  echo "Up-to-date"
+  # echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-  echo "Need to pull"
+  # echo "Need to pull"
   git pull origin master
   /home/pi/.rvm/gems/ruby-2.4.0/wrappers/jekyll build
   rm -rf /var/www/portfolio/*
   cp -r _site/* /var/www/portfolio/
 elif [ $REMOTE = $BASE ]; then
-  echo "Need to push"
+  # echo "Need to push"
 else
-  echo "Diverged"
+  # echo "Diverged"
 fi
