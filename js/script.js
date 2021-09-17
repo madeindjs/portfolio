b5e4a8bfd1de322f3b1d1aef5ce017014e06460d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-09-17 12:19:38 +0200

    add toc

--- js/script.js
+++ js/script.js
@@ -1,13 +1,58 @@
-window.onscroll = function () {
-  scrollFunction();
-};
+window.addEventListener("DOMContentLoaded", () => {
+  // REDUCE NAVBAR
 
-const navbar = document.querySelector("nav.navbar");
+  const navbar = document.querySelector("nav.navbar");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    navbar.className = "navbar lite";
-  } else {
-    navbar.className = "navbar";
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      navbar.classList.add("lite");
+    } else {
+      navbar.classList.remove("lite");
+    }
+
+    // tocEntries.forEach((entry) => {
+    //   console.log(entry.textContent);
+    // });
   }
-}
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  const tocEntries = document.querySelectorAll("ul.section-nav li");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const title = entry.target.textContent;
+      // entry.
+      if (entry.isIntersecting) {
+        console.log("%s is visible %o", title, entry.isIntersecting);
+
+        tocEntries.forEach((tocEntry) => {
+          if (tocEntry.textContent === title) {
+            tocEntry.classList.add("active");
+          } else {
+            console.log(tocEntry.textContent, title);
+            tocEntry.classList.remove("active");
+          }
+        });
+
+        // document
+        //   .querySelector(`nav li a[href="#${id}"]`)
+        //   .parentElement.classList.add("active");
+        // } else {
+        //   console.log("%s is invisible %o", title, entry.intersectionRatio);
+        // document
+        //   .querySelector(`nav li a[href="#${id}"]`)
+        //   .parentElement.classList.remove("active");
+      }
+    });
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("article h2").forEach((section) => {
+    observer.observe(section);
+  });
+});
