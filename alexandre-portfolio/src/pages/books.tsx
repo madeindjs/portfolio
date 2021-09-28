6446ba250c3bc0894007a60a14c4e758d8030623
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-09-28 17:59:07 +0200

    WIP

--- alexandre-portfolio/src/pages/books.tsx
+++ alexandre-portfolio/src/pages/books.tsx
@@ -1,78 +1,38 @@
 import {graphql} from "gatsby";
-import {Trans} from "gatsby-plugin-react-i18next";
+import {StaticImage} from "gatsby-plugin-image";
+import {Trans, useI18next} from "gatsby-plugin-react-i18next";
 import * as React from "react";
-import BookCard from "../component/BookCard";
-import Cards from "../component/Cards";
 import Layout from "../component/Layout";
 
 // markup
 const BooksPage = () => {
+  const {t} = useI18next();
   return (
     <Layout>
       <h1>
         <Trans>books</Trans>
       </h1>
-      <Cards>
-        <BookCard
-          name="API on Rails"
-          cover="../images/books/api-on-rails.svg"
-          description={
-            <>
-              <p>
-                Learn <strong>best practices</strong> to build an{" "}
-                <strong>API</strong> using <strong>Ruby on Rails</strong> 5/6.
-                The intention with this book it’s not only to teach you how to
-                build an API with Rails. The purpose is also to teach you how to
-                build <strong>scalable</strong> and{" "}
-                <strong>maintainable</strong> API with Rails which means{" "}
-                <strong>improve</strong> your current Rails knowledge.
-              </p>
-            </>
-          }
-        />
-        <BookCard
-          name="REST-API.ts"
-          cover="../images/books/rest-api-ts.svg"
-          description={
-            <>
-              <p>
-                Learn <strong>best practices</strong> to build an{" "}
-                <strong>API</strong> using <strong>Node.js / Typescript</strong>{" "}
-                and following libraries:
-              </p>
-
-              <ul>
-                <li>
-                  <a href="https://nodejs.org/en/">Node.js</a> /{" "}
-                  <a href="https://www.typescriptlang.org/">Typescript</a>
-                </li>
-                <li>
-                  <a href="https://expressjs.com/">Express.js</a> as HTTP server
-                </li>
-                <li>
-                  <a href="typeorm.io/">TypeORM</a> as ORM with{" "}
-                  <a href="https://www.sqlite.org/index.html">
-                    SQlite database
-                  </a>{" "}
-                  (but you can use Postgres or MySQL too)
-                </li>
-                <li>
-                  Test Driven Development because I think this is a best way to
-                  code
-                </li>
-              </ul>
-
-              <p>
-                The intention with this book it’s not only to teach you how to
-                build an API. The purpose is also to teach you how to build{" "}
-                <strong>scalable</strong> and <strong>maintainable</strong> API
-                with Rails which means <strong>improve</strong> your current
-                Rails knowledge.
-              </p>
-            </>
-          }
-        />
-      </Cards>
+      <p dangerouslySetInnerHTML={{__html: t("bookDescription")}}></p>
+      <section>
+        <h2>API On Rails</h2>
+        <StaticImage
+          src="../images/books/api-on-rails.svg"
+          alt={t("api-on-rails-alt")}
+          height={526}
+          width={372}
+        ></StaticImage>
+        <p>{t("api-on-rails-description")}</p>
+      </section>
+      <section>
+        <h2>REST-API.ts</h2>
+        <StaticImage
+          src="../images/books/rest-api-ts.svg"
+          alt={t("rest-api-ts-alt")}
+          height={526}
+          width={372}
+        ></StaticImage>
+        <p>{t("rest-api-ts-description")}</p>
+      </section>
     </Layout>
   );
 };
--- alexandre-portfolio/src/pages/index.tsx
+++ alexandre-portfolio/src/pages/index.tsx
@@ -1,17 +1,23 @@
 import {graphql} from "gatsby";
+import {Trans, useI18next} from "gatsby-plugin-react-i18next";
 import * as React from "react";
 import Avatar from "../component/Avatar";
 import Layout from "../component/Layout";
 
 // markup
 const IndexPage: React.FC = () => {
+  const {t} = useI18next("home");
   return (
     <Layout>
       <section>
         <Avatar></Avatar>
 
         <h1>Alexandre Rousseau</h1>
-        <p>TODO use translation</p>
+        <p>
+          <Trans>lead</Trans>
+        </p>
+        <p dangerouslySetInnerHTML={{__html: t("description")}}></p>
+        <p dangerouslySetInnerHTML={{__html: t("bookDescription")}}></p>
       </section>
     </Layout>
   );
