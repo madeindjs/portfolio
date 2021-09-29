ddf8468e7ce31d4688d9c04f2ebbf829f4b6f2ae
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2021-09-29 16:32:47 +0200

    improve footer

--- alexandre-portfolio/src/component/Footer.tsx
+++ alexandre-portfolio/src/component/Footer.tsx
@@ -1,3 +1,11 @@
+import {
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Link} from "gatsby";
 import {useI18next} from "gatsby-plugin-react-i18next";
 import React from "react";
 // @ts-ignore
@@ -32,8 +40,18 @@ const Footer: React.FC = () => {
         <p dangerouslySetInnerHTML={{__html: t("cookie")}}></p>
       </div>
       <div className={styles.externalsLinks}>
-        <p dangerouslySetInnerHTML={{__html: t("poweredByGatsby")}}></p>
-        <p dangerouslySetInnerHTML={{__html: t("licence")}}></p>
+        <Link to="https://stackoverflow.com/users/5935198/rousseaualexandre">
+          <FontAwesomeIcon icon={faStackOverflow} />
+        </Link>
+        <Link to="https://github.com/madeindjs">
+          <FontAwesomeIcon icon={faGithub} />
+        </Link>
+        <Link to="https://www.linkedin.com/in/alexandre-rousseau-a55a9464/">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </Link>
+        <Link to="mailto:alexandre@rsseau.fr">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Link>
       </div>
     </footer>
   );
