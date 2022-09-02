890254cb12ded6daab341a1f076f09ec972e2d67
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2022-09-02 14:26:04 +0200

    WIP

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,13 @@ git fetch origin
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "@{u}")
 
-if [ "$1" == "force" ]; then
-  LOCAL=""
-fi
-
-if [ $LOCAL != $REMOTE ]; then
+if [ "$LOCAL" != "$REMOTE" ] || [ "$1" = "force" ]; then
   git pull origin master
   RUBY_VERSION=$(cat .ruby-version)
   ~/.rvm/gems/$RUBY_VERSION/wrappers/jekyll build
   rm -rf /var/www/portfolio/*
   cp -r _site/* /var/www/portfolio/
   cp -r _site/.well-known /var/www/portfolio/
+else
+  echo "Deployment skipped"
 fi
